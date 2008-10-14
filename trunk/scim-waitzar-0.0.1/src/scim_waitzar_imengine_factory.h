e21b54b1c9ee544b02e7a139c651d4cc744971d8
sethhetu <sethhetu@f5f38c8e-9552-0410-b2e3-bf278a8cf01e>
2008-10-14 04:07:12 +0000

    Initial commit: reset repository because I committed a lot of useless big binary files.

---
+++ trunk/scim-waitzar-0.0.1/src/scim_waitzar_imengine_factory.h
@@ -0,0 +1,68 @@
+/*
+ *  scim_waitzar_imengine_factory.h
+ *  Copyright (C) 2008 Seth N. Hetu <sorlok_reaves@yahoo.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+/*
+ * Modified from scim_thai_imengine_factory.h in scim-thai-0.1.0.
+ * Copyright (C) 2006 Theppitak Karoonboonyanan <thep@linux.thai.net>
+ */
+
+
+#ifndef __SCIM_WAITZAR_IMENGINE_FACTORY_H__
+#define __SCIM_WAITZAR_IMENGINE_FACTORY_H__
+
+#define Uses_SCIM_ICONV
+#include <scim.h>
+#include "scim_waitzar_imengine.h"
+
+using namespace scim;
+
+class WaitZarFactory : public IMEngineFactoryBase
+{
+public:
+    WaitZarFactory (const String& uuid, const ConfigPointer& config);
+    virtual ~WaitZarFactory ();
+
+    virtual WideString  get_name      () const;
+    virtual WideString  get_authors   () const;
+    virtual WideString  get_credits   () const;
+    virtual WideString  get_help      () const;
+    virtual String      get_uuid      () const;
+    virtual String      get_icon_file () const;
+
+    virtual IMEngineInstancePointer create_instance (const String& encoding, int id = -1);
+
+private:
+    void reload_config (const ConfigPointer &config);
+
+private:
+    String m_uuid;
+
+    //
+    ConfigPointer  m_config;
+    Connection     m_reload_signal_connection;
+
+    /* for preference */
+    bool burmese_numerals;
+    int default_encoding;
+
+    /* for key bindings */
+};
+
+#endif  //__SCIM_WAITZAR_IMENGINE_FACTORY_H__
+
---
+++ trunk/src/scim_waitzar_lookup_table.h
@@ -0,0 +1,67 @@
+/*
+ *  scim_waitzar_lookup_table.h
+ *  Copyright (C) 2008 Seth N. Hetu <sorlok_reaves@yahoo.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+/*
+ * Modified from scim_native_lookup_table.h in scim-pinyin-0.5.91
+ * Copyright (c) 2005 James Su <suzhe@tsinghua.org.cn>
+ */
+
+
+#ifndef __SCIM_WAITZAR_LOOKUP_TABLE_H__
+#define __SCIM_WAITZAR_LOOKUP_TABLE_H__
+
+#include <scim.h>
+#include "waitzar-lib/WordBuilder.h"
+
+using namespace scim;
+
+/**
+ * A very simple lookup table that contains strings of Burmese words indexed from 1..10
+ */
+class WZLookupTable : public LookupTable
+{
+public:
+    WZLookupTable (int page_size = 10);
+
+   virtual void clear ();
+    
+    virtual uint32 number_of_candidates () const;
+    
+    
+    /**
+     * Get a candidate by index 
+    */
+    virtual WideString get_candidate (int index) const;
+    virtual AttributeList get_attributes (int index) const;
+
+public:
+    void setCandidates(WordBuilder *currentModelState);
+    void forceCandidate(WideString str);
+    void nullifyCandidateLabels();
+
+private:
+    std::vector<WideString>  candidate_labels;
+    std::vector<WideString>  possible_entries;
+    void regenerateCandidateLabels();
+
+};
+
+#endif //__SCIM_WAITZAR_LOOKUP_TABLE_H__
+
+
