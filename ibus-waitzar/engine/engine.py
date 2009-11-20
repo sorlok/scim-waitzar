a1b95d506d8d811551b0b5c05f60c3e288600f4f
seth.hetu <seth.hetu@f5f38c8e-9552-0410-b2e3-bf278a8cf01e>
2009-11-20 00:35:31 +0000

    Final commit for 0.0.1

--- ibus-waitzar/engine/engine.py
+++ ibus-waitzar/engine/engine.py
@@ -35,6 +35,21 @@ from ibus import modifier
 
 import libwaitzar
 
+
+#Define properties in a method similar to ibus-anthy
+ENCODING_UNICODE, \
+ENCODING_ZAWGYI, \
+ENCODING_WININNWA \
+        = range(3)
+NUMBERS_MYANMAR, \
+NUMBERS_ARABIC \
+        = range(2)
+
+
+#Must factor out later...
+PKG_DATA_DIR = u'/usr/share/ibus-waitzar/'
+
+
 log_debug = True
 
 def delete_log():
@@ -68,6 +83,26 @@ class Engine(ibus.EngineBase):
         delete_log()
         append_log("Creating engine... ibus version " + ibus.get_version())
         append_log("Time: " + str(datetime.datetime.now()))
+
+        #Set our initial encoding state
+        self.__encoding = ENCODING_UNICODE
+        self.__typenum = NUMBERS_MYANMAR
+        self.__prop_dict = {}
+        self.__prop_list = self.__init_props()
+
+        #Prop 1 - Check for saved value
+        try:
+            mode = self.__prefs.get_value('common', 'output_encoding')
+            mode = 'OutputEncoding.' + ['Unicode', 'Zawgyi', 'WinInnwa'][mode]
+            self.__output_encoding_activate(mode, ibus.PROP_STATE_CHECKED)
+        except: pass
+
+        #Prop 2 - Check for saved value
+        try:
+            mode = self.__prefs.get_value('common', 'type_numerals')
+            mode = 'TypeNumerals.' + ['Burmese', 'Arabaic'][mode]
+            self.__type_numerals_activate(mode, ibus.PROP_STATE_CHECKED)
+        except: pass
         
         self.__has_focus = False
 
@@ -81,8 +116,6 @@ class Engine(ibus.EngineBase):
 
         self.__is_invalidate = False
         self.__lookup_table = ibus.LookupTable()
-        self.__prop_list = ibus.PropList()
-        self.__prop_list.append(ibus.Property(u"Unicode 5.1", icon = u"input_uni"))
         	
         append_log("Engine created")
         
@@ -91,6 +124,7 @@ class Engine(ibus.EngineBase):
         mywordsPaths.append('/usr/share/waitzar/model2/mywords.txt')
         
         self.model = libwaitzar.WordBuilder(modelPath, mywordsPaths)
+        self.model.reset(True)
         self.sentence = libwaitzar.SentenceList()
         
         append_log("Model created")
@@ -109,6 +143,100 @@ class Engine(ibus.EngineBase):
             except:
                 append_error()
                 return False
+
+
+
+    #Handle properties
+    def property_activate(self, prop_name, state):
+        try:
+            append_log("Property activate: " + str(prop_name))
+            if state == ibus.PROP_STATE_CHECKED:
+                if self.__output_encoding_activate(prop_name, state):
+                    return
+                if self.__type_numerals_activate(prop_name, state):
+                    return
+            else:
+                if prop_name == 'setup':
+                    self.__start_setup()
+                else:
+                    self.__prop_dict[prop_name].set_state(state)
+        except:
+            append_error()
+
+
+    #Change our output encoding
+    def __output_encoding_activate(self, prop_name, state):
+        append_log("Testing output encoding... " + str(pop_name))
+        if not prop_name.startswith(u"OutputEncoding."):
+            return False
+        append_log("Yes")
+
+        #Check each encoding
+        possible_encodings = {
+            u"OutputEncoding.Unicode"   :  (ENCODING_UNICODE, u"Unicode 5.1", PKG_DATA_DIR + u"/icons/input_uni.svg"),
+            u"OutputEncoding.Zawgyi"     :  (ENCODING_ZAWGYI, u"Zawgyi-One", PKG_DATA_DIR + u"/icons/input_zg.svg"),
+            u"OutputEncoding.WinInnwa"  :  (ENCODING_WININNWA, u"Win Innwa", PKG_DATA_DIR + u"/icons/input_wi.svg")
+        }
+        if prop_name not in possible_encodings:
+            append_log("Error: Unknow prop_name = %s" % prop_name)
+            return True
+        self.__prop_dict[prop_name].set_state(state)
+        self.update_property(self.__prop_dict[prop_name])
+
+        #Now, try to react to the change
+        mode, label, icon = possible_encodings[prop_name]
+        if self.__encoding == mode:
+            return True
+
+        #Update the property
+        self.__encoding = mode
+        prop = self.__prop_dict[u"OutputEncoding"]
+        prop.label = label
+        prop.icon = icon
+        self.update_property(prop)
+
+        #Reset the model
+        self.reset()
+        self.__invalidate()
+
+
+    #Change how we type numbers
+    def __type_numerals_activate(self, prop_name, state):
+        if not prop_name.startswith(u"TypeNumerals."):
+            return False
+
+        #Check each encoding
+        numeral_types = {
+            u"TypeNumerals.Burmese"   :  (NUMBERS_MYANMAR, u"Burmese Numerals", PKG_DATA_DIR + u"/icons/num_mm.svg"),
+            u"TypeNumerals.Arabic"     :  (NUMBERS_ARABIC, u"Regular Numerals", PKG_DATA_DIR + u"/icons/num_english.svg")
+        }
+        if prop_name not in numeral_types:
+            append_log("Error: Unknow prop_name = %s" % prop_name)
+            return True
+        self.__prop_dict[prop_name].set_state(state)
+        self.update_property(self.__prop_dict[prop_name])
+
+        #Now, try to react to the change
+        mode, label, icon = numeral_types[prop_name]
+        if self.__typenum == mode:
+            return True
+
+        #Update the property
+        self.__typenum = mode
+        prop = self.__prop_dict[u"TypeNumerals"]
+        prop.label = label
+        prop.icon = icon
+        self.update_property(prop)
+
+        #Reset the model
+        self.reset()
+        self.__invalidate()
+
+
+    def __start_setup(self):
+        #Nothing for now
+        pass
+
 
     def process_key_event_internal(self, keyval, keycode, state):
         #Ignore if: 
@@ -131,7 +259,7 @@ class Engine(ibus.EngineBase):
         #First check: are we typing a letter from [a..z]? (Capitals included)
         #   If so, type this letter into our model
         #
-        letter = keyval - (keysyms.a - keysyms.A) if keyval in xrange(keysyms.A, keysyms.Z + 1) else keyval
+        letter = keyval + (keysyms.a - keysyms.A) if keyval in xrange(keysyms.A, keysyms.Z + 1) else keyval
         if letter in xrange(keysyms.a, keysyms.z + 1):
             if self.model.typeLetter(chr(letter)):
                 #Append this letter to our typed_string, and update all guesses
@@ -169,13 +297,11 @@ class Engine(ibus.EngineBase):
             if self.isGuessingWord():
                 #Shortcut to that word
                 self.pickGuess(number)
-            elif self.isHangingPhrase():
+            else:  #Always type numbers
                 #Insert that number, or its Myanmar equivalent
-                self.sentence.insert(keyval - keysysms._0)
+                self.sentence.insert(keyval - keysyms._0)
                 self.sentence.moveCursorRight(0, True, self.model)
                 self.recalcPrefixString()
-            else:
-                return False
             return True
 
         # 
@@ -477,9 +603,98 @@ class Engine(ibus.EngineBase):
     def testUpdate(self):
         self.__update()
 
+
+    def __init_props(self):
+        #Create all of our properties in one go
+        wz_props = ibus.PropList()
+
+        #
+        #Property 1: Output Encoding
+        #
+        enc_key = u"OutputEncoding"
+        encoding_prop = ibus.Property(
+                key = enc_key,
+                type = ibus.PROP_TYPE_NORMAL,
+                label = u"",
+                tooltip = u"Switch Output Encoding",
+                icon = PKG_DATA_DIR + u"/icons/input_uni.svg"
+        )
+        self.__prop_dict[enc_key] = encoding_prop
+
+        #List all properties in this menu
+        props = ibus.PropList()
+        props.append(ibus.Property(
+            key = enc_key + "." + u"Unicode",
+            type = ibus.PROP_TYPE_NORMAL,
+            label = u"Unicode 5.1",
+            icon = PKG_DATA_DIR + u"/icons/input_uni.svg"))
+        props.append(ibus.Property(
+            key = enc_key + "." + u"Zawgyi",
+            type = ibus.PROP_TYPE_NORMAL,
+            label = "Zawgyi-One",
+            icon = PKG_DATA_DIR + u"/icons/input_zg.svg"))
+        props.append(ibus.Property(
+            key = enc_key + "." + u"WinInnwa",
+            type = ibus.PROP_TYPE_NORMAL,
+            label = "Win Innwa",
+            icon = PKG_DATA_DIR + u"/icons/input_wi.svg"))
+
+        #set the state, copy all values into the main dictionary, our main menu item, and the return value
+        props[self.__encoding].set_state(ibus.PROP_STATE_CHECKED)
+        for prop in props:
+            self.__prop_dict[prop.key] = prop
+        encoding_prop.set_sub_props(props)
+        wz_props.append(encoding_prop)
+
+
+        #
+        #Property 2: Type Burmese Numerals
+        #
+        enc_key = u"TypeNumerals"
+        encoding_prop = ibus.Property(
+                key = enc_key,
+                type = ibus.PROP_TYPE_NORMAL,
+                label = u"",
+                tooltip = "Type Burmese Numerals",
+                icon = PKG_DATA_DIR + u"/icons/num_mm.svg"
+        )
+        self.__prop_dict[enc_key] = encoding_prop
+
+        #List all properties in this menu
+        props = ibus.PropList()
+        props.append(ibus.Property(
+            key = enc_key + "." + u"Burmese",
+            type = ibus.PROP_TYPE_NORMAL,
+            label = "Burmese Numerals",
+            icon = PKG_DATA_DIR + u"/icons/num_mm.svg"))
+        props.append(ibus.Property(
+            key = enc_key + "." + u"Arabic",
+            type = ibus.PROP_TYPE_NORMAL,
+            label = "Regular Numerals",
+            icon = PKG_DATA_DIR + u"/icons/num_english.svg"))
+
+        #set the state, copy all values into the main dictionary, our main menu item, and the return value
+        props[self.__encoding].set_state(ibus.PROP_STATE_CHECKED)
+        for prop in props:
+            self.__prop_dict[prop.key] = prop
+        encoding_prop.set_sub_props(props)
+        wz_props.append(encoding_prop)
+
+        #
+        #Done
+        #
+        wz_props.append(ibus.Property(key=u"setup", tooltip="Configure WaitZar"))
+        return wz_props
+
+
     def reset(self):
         #Reset the model
         self.model.reset(True)
+
+        #Make sure our encoding is up-to-date
+        currEncoding = (libwaitzar.encoding.unicode, libwaitzar.encoding.zawgyi, libwaitzar.encoding.wininnwa)[self.__encoding]
+        if (self.model.getOutputEncoding() != currEncoding):
+            self.model.setOutputEncoding(currEncoding)
         
         #Reset our array and counter
         self.sentence.clear()
